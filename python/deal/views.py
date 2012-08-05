78575e70117822643d879e269ddf36a88c5a74d8
djs22 <daniel.sharkey1@gmail.com>
2012-08-05 17:45:19 -0400

    Setup prelim deal view

--- python/deal/views.py
+++ python/deal/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import Http404
+from models import Deal
+
+
+
+def detail(request, deal_id):
+    try:
+        d = Deal.objects.get(pk=deal_id)
+    except Deal.DoesNotExist:
+        raise Http404
+    return render_to_response('deal/detail.html', {'deal': d})
